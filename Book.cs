b3accf4c74020d1b6a9cc12cb062b08c0de0d4ef
mikhail-shustov <mikhail.shustov08@gmail.com>
2025-11-06 01:25:00 +0300

    Add files via upload

---
+++ Book.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book
+{
+    internal class Book
+    {
+        private readonly string ISBN;
+
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public Book(string isbn, string title, string author)
+        {
+            ISBN = isbn;
+            Title = title;
+            Author = author;
+        }
+
+        public void BookInfo()
+        {
+            Console.WriteLine($"ISBN: {ISBN}");
+            Console.WriteLine($"Название: {Title}");
+            Console.WriteLine($"Автор: {Author}");
+        }
+    }
+}
